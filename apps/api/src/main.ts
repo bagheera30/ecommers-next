c81b0c8b64dad093ef0a31aa1cb42031dc42e56c
bagheera30 <75535876+bagheera30@users.noreply.github.com>
2023-12-06 01:02:56 +0700

    fatch(produk):create product (#1)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ProductService } from './product/product.service';
+const dotenv = require('dotenv');
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
